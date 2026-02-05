76ec9e02e88b2128b2038eedf1828ba61046e858
w1ll0w <58903678+Yung71nKzH3@users.noreply.github.com>
2026-02-05 15:44:13 +0000

    QuestionAttempts -14

--- index.js
+++ index.js
@@ -33,3 +33,72 @@ function setId(elem, str) {
     elem.id = str;
     return elem;
 }
+
+//Question 5
+function setClass(elem, str){
+    elem.className = str;
+    return elem;
+}
+
+//Question 6
+function addAClass(elem, str){
+    elem.classList.add(str);
+    return elem;
+}
+
+//Question 7
+function removeAClass(elem, str){
+    elem.classList.remove(str);
+    return elem;
+}
+
+//Question 8
+function newElement(name){
+    return document.createElement(name);
+}
+
+//Question 9
+function findElementById(id){
+    return document.querySelector(`#${id}`);
+}
+
+//Question 10
+function findElementsByQuery(query){
+    return document.querySelectorAll(query)
+}
+
+//Question 11
+function reverseList(query){
+    const list = document.querySelector(query);
+    const items = Array.from(list.children);
+    items.reverse();
+    items.forEach(item => list.appendChild(item));
+    return list;
+}
+
+//Question 12
+function mover(moveThis, appendToThis) {
+    const elementToMove = document.querySelector(moveThis);
+    const target = document.querySelector(appendToThis);
+    target.appendChild(elementToMove);
+    return target;
+}
+
+//Question 13
+function filler(list, candidates) {
+    candidates.forEach(str => {
+        const li = document.createElement("li");
+        li.textContent = str;
+        list.appendChild(li);
+    });
+    return list;
+}
+
+//Question 14
+function dupe(selector) {
+    const original = document.querySelector(selector);
+    const copy = original.cloneNode(true);
+    original.parentNode.appendChild(copy);
+    return original;
+}
+
